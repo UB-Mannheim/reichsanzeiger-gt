7601ebefaf5d3bd29249b0d1603b2a9c473894be
Thomas Schmidt <thomas.schmidt@bib.uni-mannheim.de>
2022-12-12 10:24:56 +0100

    Added complete Reichsanzeiger GT 1820-1939 with and without TableRegion

---
+++ data/download_images.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+
+urlbase=`echo "aHR0cHM6Ly9kaWdpLmJpYi51bmktbWFubmhlaW0uZGUvcmVpY2hzYW56ZWlnZXIuZmNnaT9GSUY9
+L3JlaWNoc2FuemVpZ2VyL2ZpbG0vCg==" | base64 -d`
+cat imageurls.list | while read -r line; do wget --limit-rate=500k "${urlbase}${line% *}" -O ${line#* }; done
