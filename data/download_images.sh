e1f5eda157ed4d845e8f66dd2070e99a52013dbd
Stefan Weil <sw@weilnetz.de>
2023-05-12 17:41:13 +0200

    Update download script
    Don't download existing page images again.
    
    Signed-off-by: Stefan Weil <sw@weilnetz.de>

--- data/download_images.sh
+++ data/download_images.sh
@@ -2,4 +2,4 @@
 mkdir images
 urlbase=`echo "aHR0cHM6Ly9kaWdpLmJpYi51bmktbWFubmhlaW0uZGUvcmVpY2hzYW56ZWlnZXIuZmNnaT9GSUY9
 L3JlaWNoc2FuemVpZ2VyL2ZpbG0vCg==" | base64 -d`
-cat imageurls.list | while read -r line; do wget --limit-rate=500k "${urlbase}${line% *}" -O ./images/${line#* }; done
+cat imageurls.list | while read -r line; do test -f ${line#* } || wget --limit-rate=50000k "${urlbase}${line% *}" -O images/${line#* }; done
